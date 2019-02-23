b4fd2b2ad92f705093de1a8f9cfe5799e20726ee
zhanmian <zhanmian@yeah.net>
2019-02-23 23:38:26 +0800

    查询订单详情

---
+++ src/components/product/AddProduct.vue
@@ -0,0 +1,15 @@
+<template>
+  <product-detail :is-edit='false'></product-detail>
+</template>
+
+<script>
+  import ProductDetail from './ProductDetail'
+  export default {
+    name: 'addProduct',
+    components: { ProductDetail }
+  }
+</script>
+
+<style scoped>
+
+</style>
